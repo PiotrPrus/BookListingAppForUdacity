1df98ba3ff2f236510ea75d8150f5c72c4fcdf64
piotr.prus <piotr.prus@fancyfon.com>
2017-07-28 10:47:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/pprus/booklistingappforudacity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.pprus.booklistingappforudacity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import static android.R.attr.id;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
